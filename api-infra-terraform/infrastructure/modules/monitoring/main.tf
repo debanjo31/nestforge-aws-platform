6f0972fbec4a3f6ed221621cea76b797775f53f4
israel <debanjo31@gmail.com>
2026-09-07 23:32:20 +0100

    feat: remove unused Terraform monitoring and networking modules

--- api-infra-terraform/infrastructure/modules/monitoring/main.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_cloudwatch_log_group" "ecs_api" {
-  name              = "/ecs/${var.name_prefix}-api"
-  retention_in_days = var.log_retention_days
-
-  tags = {
-    Name = "${var.name_prefix}-api-logs"
-  }
-}
